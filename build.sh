e1979d5c5de08b9abd93fed701a7c1db682fe592
sbencoding <mcbenighost@gmail.com>
2019-12-23 16:26:07 +0100

    Rename build output to wd_bridge

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #g++ wd_bridge.cpp wdfs.cpp bridge.cpp -o sfs2 `pkg-config fuse3 --cflags --libs && curl-config --libs`
-clang++ wd_bridge.cpp wdfs.cpp bridge.cpp -o sfs2 `pkg-config fuse3 --cflags --libs && curl-config --libs`
+clang++ wd_bridge.cpp wdfs.cpp bridge.cpp -o wd_bridge `pkg-config fuse3 --cflags --libs && curl-config --libs`
