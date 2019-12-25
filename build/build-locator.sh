670dc036a5ef695acf3bdf04abffb9884f1c12d1
sbencoding <mcbenighost@gmail.com>
2019-12-26 00:46:07 +0100

    Update build related stuff
    Update depencencies in readme file
    Add option to script to build with g++
    Add option to script to build for 32-bit systems (required by
    raspberrypi for example)

--- build/build-locator.sh
+++ build/build-locator.sh
@@ -1,1 +1,10 @@
-clang++ ../src/device_locator.cpp ../src/bridge.cpp -o ../bin/device_locator `curl-config --libs`
+COMPILER="clang++"
+FLAGS="../src/device_locator.cpp ../src/bridge.cpp -o ../bin/device_locator `curl-config --libs`"
+if [ "$1" = "gcc" ]
+then
+    echo "building using gcc"
+    COMPILER="g++ -Wno-psabi"
+else
+    echo "building using clang"
+fi
+$COMPILER $FLAGS
--- build/build.sh
+++ build/build.sh
@@ -1,2 +1,16 @@
-#g++ wd_bridge.cpp wdfs.cpp bridge.cpp -o sfs2 `pkg-config fuse3 --cflags --libs && curl-config --libs`
-clang++ ../src/wd_bridge.cpp ../src/wdfs.cpp ../src/bridge.cpp -o ../bin/wd_bridge `pkg-config fuse3 --cflags --libs && curl-config --libs`
+COMPILER="clang++"
+FLAGS="../src/wd_bridge.cpp ../src/wdfs.cpp ../src/bridge.cpp -o ../bin/wd_bridge `pkg-config fuse3 --cflags --libs && curl-config --libs`"
+ARCH_FLAGS=""
+if [ "$1" = "gcc" ]
+then
+    echo "building using gcc"
+    COMPILER="g++ -Wno-psabi"
+else
+    echo "building using clang"
+fi
+if [ "$2" = "32" ]
+then
+    echo "using 32bit arch flags"
+    ARCH_FLAGS="-D_FILE_OFFSET_BITS=64"
+fi
+$COMPILER $FLAGS $ARCH_FLAGS
